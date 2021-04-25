73b6d7c5a47e3d4ce58a5fc257529175f42dec66
高文峰 <1152057576@qq.com>
2021-04-25 10:02:37 +0800

    Update secure.py

--- app/secure.py
+++ app/secure.py
@@ -10,7 +10,7 @@ PORT = 8090
 SQLALCHEMY_DATABASE_URI = "mysql+cymysql://root:root@localhost:3306/fisher"
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = "AIJD1314"
+SECRET_KEY = ""
 
 # email配置
 MAIL_SERVER = 'smtp.qq.com'
@@ -19,6 +19,6 @@ MAIL_USE_SSL = True
 MAIL_USE_TSL = False
 MAIL_USERNAME = '1152057576@qq.com'
 # QQ邮箱->设置->账户->[POP3...]->生成授权码->发送短信->获取授权码
-MAIL_PASSWORD = 'pstomjiomwyybadh'
+MAIL_PASSWORD = ''
 
 
